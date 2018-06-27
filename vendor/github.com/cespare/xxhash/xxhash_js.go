a662f9052c2c1d64a8d3b2dc32164822cbbd2eeb
David Brinegar <david.brinegar@lightbend.com>
2018-06-27 08:45:54 -0700

    h32 hash 10% faster than h64

--- vendor/github.com/cespare/xxhash/xxhash_js.go
+++ vendor/github.com/cespare/xxhash/xxhash_js.go
@@ -12,6 +12,7 @@ func writeBlocks(x *xxh, b []byte) (res []byte) {
 	return
 }
 
+// using the js xxh lib gives 2x speedup over the go version
 func h64(b []byte) uint64 {
 	hash := js.Global.Get("XXH").Call("h64", string(b), 0)
 	return uint64(hash.Get("_a00").Uint64() +
@@ -20,6 +21,8 @@ func h64(b []byte) uint64 {
 		hash.Get("_a48").Uint64()<<48)
 }
 
+// this is 10% faster than h64 running "demo(4)"
 func h32(b []byte) uint64 {
-	return uint64(js.Global.Get("XXH").Call("h64", string(b), 0).Int())
+	lower := uint32(js.Global.Get("XXH").Call("h32", string(b), 0).Int())
+	return uint64(lower) + uint64(lower)<<32
 }
